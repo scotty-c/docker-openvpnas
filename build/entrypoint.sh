adf2d7d817030987cabcdbb7714ab0e65be35c18
Scott Coulton <scott.coulton@gmail.com>
2016-09-26 14:11:20 +1000

    fix for twistd.pid being left behind after shutdown

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# remove twisted pid
+if ps -p $(cat twistd.pid) > /dev/null 2>&1
+then
+    kill $(cat twistd.pid)
+    /usr/local/openvpn_as/scripts/openvpnas -n
+    exit 0
+else
+    echo "no twistd.pid found"   
+fi
+
 # remove pid file if it exists
 if [ -e "/var/run/openvpnas.pid" ]; then
   rm -f "/var/run/openvpnas.pid" &>/dev/null
